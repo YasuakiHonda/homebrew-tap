46cec1b41ee2f06e988a3cc3ef631948ef64671a
Yasuaki Honda <yasuaki.honda@gmail.com>
2022-07-30 09:29:36 +0900

    Update maxima-jupyter-installer.rb

--- Formula/maxima-jupyter-installer.rb
+++ Formula/maxima-jupyter-installer.rb
@@ -1,7 +1,7 @@
 class MaximaJupyterInstaller < Formula
   desc "Installer shell script of maxima-jupyter kernel"
   homepage "https://github.com/YasuakiHonda/maxima-jupyter-installer"
-  url "https://github.com/YasuakiHonda/maxima-jupyter-installer/archive/refs/tags/v1.1.tar.gz"
+  url "https://github.com/YasuakiHonda/maxima-jupyter-installer/archive/refs/tags/v1.2.tar.gz"
   sha256 "466081bb126a749eed0e584ef8bc99210b877f3374ac78da01f8da22b4473d2f"
 
   depends_on "curl"
